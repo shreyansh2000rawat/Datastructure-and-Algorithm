ee1b199d11e074efd49a58a0f17d9e82c5cada7b
shreyansh2000rawat <54751897+shreyansh2000rawat@users.noreply.github.com>
2020-12-25 12:04:21 +0530

    Add files via upload

---
+++ DP/largest palindrom subsequence/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string x;
+    cin>>x;
+    string y = x;
+    reverse(y.begin(),y.end());
+    int m = x.length();
+    int n = y.length();
+    int t[m+1][n+1];
+    int i,j;
+    for(i=0;i<=m;i++)
+    {
+        for(j=0;j<=n;j++)
+        {
+            if(i==0 || j==0)
+            {
+                t[i][j] = 0;
+            }
+            else if(x[i-1] == y[j-1])
+            {
+                t[i][j] = t[i-1][j-1] + 1;
+            }
+            else
+            {
+                t[i][j] = max(t[i-1][j],t[i][j-1]);
+            }
+        }
+    }
+    cout<<t[m][n]<<endl;
+    return 0;
+}
